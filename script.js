bf16b4fb8544081db207dc88e3a000a7be1f296a
BejotaTigre3 <143463264+BejotaTigre3@users.noreply.github.com>
2023-09-21 15:13:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const sounds = ['Young fly on the track']
 
+sounds.forEach(sound => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+
+  document.getElementById('buttons')
+  appendChild(btn)
+})
